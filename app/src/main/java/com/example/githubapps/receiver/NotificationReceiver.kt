f945a94259791ed73888d74e2a564ae184748611
Ridho <ridhowildan19@gmail.com>
2020-05-22 20:05:51 +0900

    Add Content Intent for back to activity when notification Clicked

--- app/src/main/java/com/example/githubapps/receiver/NotificationReceiver.kt
+++ app/src/main/java/com/example/githubapps/receiver/NotificationReceiver.kt
@@ -2,16 +2,23 @@ package com.example.githubapps.receiver
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.example.githubapps.R
+import com.example.githubapps.view.search.SearchUser
+
 
 class NotificationReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent?) {
+
+        val someIntent = Intent(context, SearchUser::class.java)
+        val pIntent = PendingIntent.getActivity(context, 0, someIntent, 0)
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val name = context.getString(R.string.reminder)
             val descriptionText = context.getString(R.string.alarmMessage)
@@ -27,14 +34,11 @@ class NotificationReceiver : BroadcastReceiver() {
             .setContentTitle(context.getString(R.string.reminder))
             .setContentText(context.getString(R.string.alarmMessage))
             .setAutoCancel(true)
+            .setContentIntent(pIntent)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
         val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val id = System.currentTimeMillis() / 1000
         nm.notify(id.toInt(), mBuilder.build())
     }
-
-
-
-
 }
